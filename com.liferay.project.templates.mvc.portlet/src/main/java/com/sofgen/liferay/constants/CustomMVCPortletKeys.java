3d044640b36cc62a306f49893679545f7c7f3452
Al Francis Retumban <aretumban@sofgen.com>
2017-07-26 13:15:55 +0800

    initial commit - project setup

---
+++ com.liferay.project.templates.mvc.portlet/src/main/java/com/sofgen/liferay/constants/CustomMVCPortletKeys.java
@@ -0,0 +1,10 @@
+package com.sofgen.liferay.constants;
+
+/**
+ * @author sofgen
+ */
+public class CustomMVCPortletKeys {
+
+	public static final String CustomMVC = "CustomMVC";
+
+}
---
+++ com.liferay.project.templates.mvc.portlet/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="com.liferay.project.templates.mvc.portlet.caption"/></b>
+</p>
